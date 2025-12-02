a735cc91fc41518d4e40c048f87699606c1e00b4
codejutsu1 <codejutsu@protonmail.com>
2025-12-02 14:45:29 +0100

    fix: Remove @routes directive and rebuild assets for homepage styling
    - Remove @routes Blade directive (Ziggy not installed)
    - Fix duplicated HTML structure in app.blade.php
    - Rebuild Vite assets to resolve styling issues
    - Homepage now displays correctly with all styles applied

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -12,7 +12,6 @@
     <link href="https://fonts.bunny.net/css?family=inter:400,500,600,700,800" rel="stylesheet" />
 
     <!-- Scripts -->
-    @routes
     @vite(['resources/js/app.js', "resources/js/Pages/{$page['component']}.vue"])
     @inertiaHead
 </head>
